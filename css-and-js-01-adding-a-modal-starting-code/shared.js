208021e786cb46eb715163accf3deb5622d71680
dhk939393 <dhk9309@gmail.com>
2024-08-25 22:09:59 +0900

    CSS

--- css-and-js-01-adding-a-modal-starting-code/shared.js
+++ css-and-js-01-adding-a-modal-starting-code/shared.js
@@ -1,7 +1,9 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let selectPlanButton = document.querySelectorAll('.plan button');
-let togg
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav')
+let modalNoButton = document.querySelector('.modal__action--negative');
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click',function() {
@@ -9,3 +11,21 @@ for (let i = 0; i < selectPlanButton.length; i++) {
         backdrop.style.display = 'block';
     })
 }
+
+backdrop.addEventListener("click", function(){
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener("click",closeModal)
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+
+})
